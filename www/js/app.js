f807aa30b4b4a0a6fa17cb6d7dfb6d32c10ead03
Dean Wong <Dean Wong>
2016-06-08 18:58:43 -0400

    tries ngcordova, switch to phonegap with ionic

--- www/js/app.js
+++ www/js/app.js
@@ -3,42 +3,57 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var ionicApp = angular.module('starter', ['ionic']);
-ionicApp.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if(window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
-});
-
-ionicApp.controller('MyCtrl', function($scope) {
-  $scope.groups = [];
-  for (var i=0; i<5; i++) {
-    $scope.groups[i] = {
-      name: i,
-      items: []
+angular.module('starter', ['ionic', 'ngCordova'])
+// .run(function($ionicPlatform) {
+//     $ionicPlatform.ready(function() {
+//         if(window.cordova && window.cordova.plugins.Keyboard) {
+//             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//         }
+//         if(window.StatusBar) {
+//             StatusBar.styleDefault();
+//         }
+//     });
+// })
+.controller('MyCtrl', function($scope) {
+    $scope.groups = [];
+    for (var i=0; i<5; i++) {
+      $scope.groups[i] = {
+        name: i,
+        items: []
+      };
+      for (var j=0; j<3; j++) {
+        $scope.groups[i].items.push(i + '-' + j);
+      }
+    }
+     /*
+     * if given group is the selected group, deselect it
+     * else, select the given group
+     */
+    $scope.toggleGroup = function(group) {
+      if ($scope.isGroupShown(group)) {
+        $scope.shownGroup = null;
+      } else {
+        $scope.shownGroup = group;
+      }
     };
-    for (var j=0; j<3; j++) {
-      $scope.groups[i].items.push(i + '-' + j);
-    }
-  }
-
-  /*
-   * if given group is the selected group, deselect it
-   * else, select the given group
-   */
-  $scope.toggleGroup = function(group) {
-    if ($scope.isGroupShown(group)) {
-      $scope.shownGroup = null;
-    } else {
-      $scope.shownGroup = group;
-    }
-  };
-  $scope.isGroupShown = function(group) {
-    return $scope.shownGroup === group;
-  };
+    $scope.isGroupShown = function(group) {
+      return $scope.shownGroup === group;
+    };
+})
+.controller("MyCalendar", function($ionicPlatform, $scope, $cordovaCalendar) {
+  $ionicPlatform.ready(function() {
+    $scope.createEvent = function() {
+        $cordovaCalendar.createEvent({
+            title: 'Space Race',
+            location: 'The Moon',
+            notes: 'Bring sandwiches',
+            startDate: new Date(2016, 5, 8, 18, 30, 0, 0, 0),
+            endDate: new Date(2016, 5, 8, 20, 0, 0, 0, 0)
+        }).then(function (result) {
+            console.log("Event created successfully");
+        }, function (err) {
+            console.error("There was an error: " + err);
+        });
+    };
+  });
 });
